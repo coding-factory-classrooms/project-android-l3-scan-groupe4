53cddf06118a8f14a5664c0db0c01f2e687a773d
Micael <micael.dossantos@edu.itescia.fr>
2021-03-11 20:52:53 +0100

    page detail of item done

---
+++ MiamScan/app/src/main/java/com/example/miamscan/FoodDetailsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.miamscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.miamscan.databinding.ActivityFoodDetailsBinding
+import com.squareup.picasso.Picasso
+
+class FoodDetailsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFoodDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFoodDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewNameDetail.text = getIntent().getStringExtra("foodName")
+        binding.textViewBrandDetail.text = getIntent().getStringExtra("foodBrand")
+        Picasso.get().load(getIntent().getStringExtra("foodImage")).into(binding.imageViewDetails)
+    }
+}
